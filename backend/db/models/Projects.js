56df84602b5a3341a15a9e16c9fb174a2d8a154e
Ola <aleksandra@gruchala.eu>
2019-12-06 17:53:41 +0100

    Add schema for Projects

---
+++ backend/db/models/Projects.js
@@ -0,0 +1,33 @@
+const {
+  Schema
+} = require('mongoose');
+
+const ObjectId = Schema.Types.ObjectId;
+
+const Projects = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String
+  },
+  cratedAt: {
+    type: Date
+  },
+  deadline: {
+    type: Date
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  contributors: {
+    type: String
+  }
+})
+
+module.exports = {
+  Projects
+};
