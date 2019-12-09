2ec76ed8357c6ec892dffd256cd6e47ab8dfaf66
edych <edytapatrycja.chrzanowska@gmail.com>
2019-12-09 20:45:35 +0100

    added features schema

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./User');
 const Project = require('./Project');
 const Milestone = require('./Milestone');
+const Feature = require('./Feature');
 
 module.exports = {
   User,
   Project,
   Milestone,
+  Feature,
 };
