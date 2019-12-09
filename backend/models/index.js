f3be3c29a53d158325fc91d3a4eec80c57802bc4
Adrian <adrian.halawczak@gmail.com>
2019-12-09 20:44:13 +0100

    subtickets added

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,12 @@ const Demo = require('./Demo');
 const User = require('./User');
 const Project = require('./Project');
 const Milestone = require('./Milestone');
+const Subticket = require('./Subticket');
 
 module.exports = {
   Demo,
   User,
   Project,
   Milestone,
+  Subticket,
 };
