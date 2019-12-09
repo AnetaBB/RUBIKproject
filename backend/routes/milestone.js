d18cfab02e78ff1928c256e9f56bf23e72843cae
JoannaDS <j.dabrowska.slaby@gmail.com>
2019-12-09 20:21:03 +0100

    Update milestone.js

--- backend/routes/milestone.js
+++ backend/routes/milestone.js
@@ -6,12 +6,7 @@ router.get('/', async (req, res) => {
   const milestones = await Milestone.find();
   res.sendStatus(200).send(milestones);
 });
-// console.log(
-//   req.query,
-//   milestones.map(milestone => {
-//     return milestone;
-//   })
-// );
+
 router.post('/', async (req, res) => {
   const { Milestone } = res.locals.models;
   const milestones = await Milestone.find();
@@ -19,7 +14,7 @@ router.post('/', async (req, res) => {
   if (!req.body) return res.status(400).send('0 in body');
 
   const milestone = new Milestone({
-    id: milestones.length + 1,
+    // id: milestones.length + 1,
     title: req.body.title,
     description: req.body.description,
     owner: req.body.owner,
@@ -28,6 +23,12 @@ router.post('/', async (req, res) => {
 
   milestones.push(milestone);
   res.sendStatus(200).send(milestone);
+  console.log(
+    req.query,
+    milestones.map(milestone => {
+      return milestone;
+    })
+  );
 });
 
 module.exports = router;
