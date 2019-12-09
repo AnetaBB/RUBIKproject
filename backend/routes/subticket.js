89c62e6ae6a098e4e9d89514e9fee9e8262d4260
Adrian <adrian.halawczak@gmail.com>
2019-12-09 17:56:08 +0100

    subticket router

---
+++ backend/routes/subticket.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const {
+    Subticket
+  } = res.locals.models;
+  const subtickets = await Subticket.find();
+  res.sendStatus(200).send('Subtickets');
+  console.log(
+    req.query,
+    subtickets.map(subticket => {
+      return subticket;
+    })
+  );
+});
+
+module.exports = router;
