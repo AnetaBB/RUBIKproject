c7cf4ef5b68a81bcdc3532b59c258e955f72e88e
JoannaDS <j.dabrowska.slaby@gmail.com>
2019-12-10 23:08:36 +0100

    Winner!

--- backend/routes/milestone.js
+++ backend/routes/milestone.js
@@ -25,6 +25,19 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const { Milestone } = res.locals.models;
+    if (!req.params.id) return res.status(400).send('0 in body');
+    const milestone = await Milestone.findById(req.params.id);
+    milestone.title = req.body.title;
+    const result = await milestone.save();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).send(`Hey dude this is the reason:${error}`);
+  }
+});
+
 router.delete('/:id', async (req, res) => {
   const { Milestone } = res.locals.models;
   const milestone = await Milestone.findByIdAndRemove(req.params.id);
