9a342d0815651fdc3d77980740b46fea6f9ba757
Aneta <a.banas.brejt@gmail.com>
2019-12-10 23:38:06 +0100

    putFix'

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -34,4 +34,17 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  if (!req.params.id) res.status(404).send('There is no such comment');
+  try {
+    const { Comment } = res.locals.models;
+    const comment = await Comment.findById(req.params.id);
+    comment.status = req.body.status;
+    const result = await comment.save();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(404).send(`There is no such comment:${err}`);
+  }
+});
+
 module.exports = router;
