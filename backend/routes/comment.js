f53bc4addfb99ac911aa507aa01241c6b71b839c
MareK <markust71@gmail.com>
2019-12-11 00:32:17 +0100

    and fix

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -23,6 +23,7 @@ router.post('/', async (req, res) => {
     res.status(400).send(`Adding comment failed with error: ${err}`);
   }
 });
+
 router.delete('/:id', async (req, res) => {
   try {
     const { Comment } = res.locals.models;
@@ -31,6 +32,19 @@ router.delete('/:id', async (req, res) => {
     res.status(200).json(result);
   } catch (err) {
     res.status(400).send(`Deleting failed with error: ${err}`);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  if (!req.params.id) res.status(404).send('There is no such comment');
+  try {
+    const { Comment } = res.locals.models;
+    const comment = await Comment.findById(req.params.id);
+    comment.status = req.body.status;
+    const result = await comment.save();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(404).send(`There is no such comment:${err}`);
   }
 });
 
