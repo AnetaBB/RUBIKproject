a1862c445657b1b8647c0173f81dbe223ac44f23
unknown <luiza.michalska@op.pl>
2019-12-12 00:15:19 +0100

    endpoints

--- backend/routes/ticket.js
+++ backend/routes/ticket.js
@@ -3,14 +3,50 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const { Ticket } = res.locals.models;
-  const tickets = await Ticket.find();
-  res.sendStatus(200);
-  console.log(
-    req.query,
-    tickets.map(ticket => {
-      return ticket;
-    })
-  );
+  const ticket = await Ticket.find();
+  res.status(200).json(ticket);
+});
+
+router.get('/:id', async (req, res) => {
+  const { Ticket } = res.locals.models;
+  const ticket = await Ticket.findById(req.params.id);
+  res.status(200).json(ticket);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { Ticket } = res.locals.models;
+    if (!req.body) return res.status(400).send('Bad request');
+    const ticket = new Ticket(req.body);
+    const result = await ticket.save();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).send(`Adding ticket failed: ${err}`);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { Ticket } = res.locals.models;
+    if (!req.params.id) return res.status(400).send('Bad request');
+    const ticket = await Ticket.findById(req.params.id);
+    ticket.title = req.body.title;
+    const result = await ticket.save();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).send(`Updating ticket failed: ${err}`);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { Ticket } = res.locals.models;
+    const ticket = await Ticket.findByIdAndRemove(req.params.id);
+    const result = await ticket.delete();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).send(`Deleting failed: ${err}`);
+  }
 });
 
 module.exports = router;
