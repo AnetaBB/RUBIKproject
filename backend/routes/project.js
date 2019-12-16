1ec0ffdfc185c342a894d481ce36532137d12131
Ola <45734574+olagruchala@users.noreply.github.com>
2019-12-16 11:12:13 +0100

    Ola/project routes (#14) Add http methods for Project collection
    * Add not working http post method for Project collection
    
    * Add http post and put method for Project collection
    
    * Add http delete method for Project collection
    
    * Fix to http delete method for Project collection

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -32,7 +32,28 @@ router.put('/:id', async (req, res) => {
   if (!project) return;
 
   const bodyElements = [];
-  for (let el in Project.schema.obj){
+  for (const el in Project.schema.obj){
+    bodyElements.push(el)
+  }
+
+  // todo: validation what is required
+
+  project
+    .save()
+    .then(res.status(200).json(project))
+    .catch(error => {
+      res.status(400).send('Editing project failed: ' + error);
+    })
+});
+
+router.patch('/:id', async (req, res) => {
+  const { Project } = res.locals.models;
+  if (!req.body) return res.status(400).send('Bad request');
+  const project = await Project.findById(req.params.id);
+  if (!project) return;
+
+  const bodyElements = [];
+  for (const el in Project.schema.obj){
     bodyElements.push(el)
   }
   bodyElements.forEach( prop => {
@@ -50,5 +71,17 @@ router.put('/:id', async (req, res) => {
     .catch(error => {
       res.status(400).send('Editing project failed: ' + error);
     })
+});
+
+router.delete('/:id', async (req, res) => {
+    const { Project } = res.locals.models;
+    const project = await Project.findById(req.params.id);
+    if (!project) return res.status(404).send('This project was not found');
+    project
+      .delete()
+      .then(res.status(200).json(project))
+      .catch(error => {
+        res.status(400).send('Deleting project failed: ' + error);
+      })
 });
 module.exports = router;
