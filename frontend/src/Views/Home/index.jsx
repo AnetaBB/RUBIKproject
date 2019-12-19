99fafd8bcc38412ee99643f8c67b83001fe99c36
kontaktzajac <kontakt.zajac@gmail.com>
2019-12-19 15:57:27 +0100

    View content

--- frontend/src/Views/Home/index.jsx
+++ frontend/src/Views/Home/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import Sidebar from '../../components/Home/Sidebar';
 import TopNavbar from '../../components/Home/TopNavbar';
+import ExampleCard1 from '../../components/Home/ExampleCard1';
 //import { ReactComponent } from '*.svg';
 
-const Home = () => {
-  return (
-    <>
-      <div id="wrapper">
-        <Sidebar />
-        <div id="content-wrapper" className="d-flex flex-column">
-          <div id="content">
-            <TopNavbar />
+class Home extends React.Component {
+  state = { content: '' };
+
+  selectContent = wartosc => {
+    console.log(typeof wartosc);
+    this.setState({ content: wartosc });
+    console.log(this.state.content);
+  };
+
+  renderContent() {
+    console.log('123');
+    if (this.state.content === 'project') {
+      console.log('w srodku');
+      return <ExampleCard1 />;
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div id="wrapper">
+          <Sidebar mojaFunkcja={this.selectContent} />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <TopNavbar />
+              {this.renderContent()}
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Home;
---
+++ frontend/src/components/Home/ExampleCard1.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+class ExampleCard1 extends React.Component {
+  render() {
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button
+            variant="primary"
+            onClick={() => {
+              this.setState({ content: 'to-do-user' });
+            }}
+          >
+            Go somewhere
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default ExampleCard1;
