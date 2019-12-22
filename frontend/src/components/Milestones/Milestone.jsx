98395f4fd5987144304634ae3063eae6365b0efa
JoannaDS <j.dabrowska.slaby@gmail.com>
2019-12-22 20:42:13 +0100

    Updading Milestone

--- frontend/src/components/Milestones/Milestone.jsx
+++ frontend/src/components/Milestones/Milestone.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Table from 'react-bootstrap/Table';
 
 class Milestone extends React.Component {
   render() {
