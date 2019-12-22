470aac3639632f2642040d778aa33622193fb0b6
Adrian <adrian.halawczak@gmail.com>
2019-12-22 23:15:47 +0100

    subticket schema expanded

--- backend/models/Subticket.js
+++ backend/models/Subticket.js
@@ -2,20 +2,49 @@ const { Schema } = require('mongoose');
 const ObjectId = Schema.Types.ObjectId;
 
 const Subticket = new Schema({
+  // ticket: {
+  //   type: ObjectId,
+  //   ref: 'Ticket',
+  //   required: true,
+  // },
   title: {
     type: String,
-    required: true,
     unique: true,
+    minlength: 5,
+    maxlength: 70,
+    required: true,
   },
   description: {
     type: String,
+    minlength: 25,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
     required: true,
   },
   status: {
     type: String,
     default: 'Open',
-    required: true,
     enum: ['Open', 'Assigned', 'Completed', 'Closed'],
+    required: true,
+  },
+  contributor: {
+    type: String,
+    // docelowo wykonawca nadrzednego ticketa
+    default: 'Mateusz',
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High', 'Urgent'],
+    required: true,
+  },
+  relevance: {
+    type: String,
+    enum: ['Trivial', 'Minor', 'Major', 'Critical'],
+    required: true,
   },
 });
 
