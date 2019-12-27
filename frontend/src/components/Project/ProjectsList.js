4ebbf15c5a65fcb4b7f18b9e49fcdb0af6edd4f2
Ola <aleksandra@gruchala.eu>
2019-12-27 19:03:58 +0100

    Add error handler to fetching projects

--- frontend/src/components/Project/ProjectsList.js
+++ frontend/src/components/Project/ProjectsList.js
@@ -3,32 +3,41 @@ import { Context } from '../../Store';
 
 const ProjectsList = props => {
   const [projects, setProjects] = useState([]);
+  const [error, setError] = useState('');
 
   let context = useContext(Context);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/projects`)
+    fetch(`/api/projects`)
       .then(result => result.json())
       .then(projects => {
         setProjects(projects);
-      });
-  });
+      })
+      .catch(error => setError(error));
+  }, []);
 
-  return projects.map(project => {
+  if (error) {
+    return <span>{error.toString()}</span>;
+  } else {
     return (
-      <span
-        key={project._id}
-        id={project._id}
-        className="collapse-item"
-        onClick={() => {
-          context.changeStore('projectID', project._id);
-          props.changeContent('project');
-        }}
-      >
-        {project.title}
-      </span>
+      projects &&
+      projects.map(project => {
+        return (
+          <span
+            key={project._id}
+            id={project._id}
+            className="collapse-item"
+            onClick={() => {
+              context.changeStore('projectID', project._id);
+              props.changeContent('project');
+            }}
+          >
+            {project.title}
+          </span>
+        );
+      })
     );
-  });
+  }
 };
 
 export default ProjectsList;
