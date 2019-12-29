9b0bf582a039deaaf3bcdd6f9bab624467f45f33
Joanna DS <j.dabrowska.slaby@gmail.com>
2019-12-29 22:06:57 +0100

    Adding owner and contributor

--- frontend/src/components/Milestones/AddAMilestone.jsx
+++ frontend/src/components/Milestones/AddAMilestone.jsx
@@ -1,66 +1,60 @@
-
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Table from 'react-bootstrap/Table';
-
-class AddAMilestone extends React.Component {
-  state = { usersApi: [] };
-
-  async componentDidMount() {
-    const response = await fetch('http://127.0.0.1:8080/api/milestones');
-    const data = await response.json();
-    this.setState({ usersApi: data });
-
-  }
-  render() {
-    return (
-      <div className="row">
-        <div className="col-lg-7">
-        <Card style={{ width: '20rem' }}>
-            <Card.Body>
-              <Card.Title>Milestone</Card.Title>
-              <Card.Text>
-                Estimates main milestones to finish your project!
-              </Card.Text>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                </InputGroup.Prepend>
-                <FormControl aria-label="Text input with checkbox" />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                </InputGroup.Prepend>
-                <FormControl aria-label="Text input with checkbox" />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                </InputGroup.Prepend>
-                <FormControl aria-label="Text input with checkbox" />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                </InputGroup.Prepend>
-                <FormControl aria-label="Text input with checkbox" />
-              </InputGroup>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          
-        </div>
-      </div>
-    );
-  }
-}
-
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+const AddAMilestone = () => {
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = e => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    setValidated(true);
+  };
+  return (
+    <div className='container'>
+      <h2>Add a new milestone</h2>
+              
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group controlId='milestoneTitle'>
+          <Form.Label>Milestone Title</Form.Label>            
+          <Form.Control required type='text' placeholder='Milestone title' />
+           
+          <Form.Control.Feedback type='invalid'>
+            Please fill out the milestone            
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group controlId='milestoneDescription'>
+          <Form.Label>Description</Form.Label>            
+          <Form.Control
+            as='textarea'
+            rows='7'
+            placeholder='Describe the main milestones you need to reach to finish your project!'
+          />
+        </Form.Group>
+        <Form.Group controlId='milestoneOwner'>
+          <Form.Label>Choose owner</Form.Label>
+          <Form.Control as='select'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId='milestoneContributors'>
+          <Form.Label>Choose contributor</Form.Label>
+          <Form.Control as='select'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>{' '}
+        <Button type='submit'>Submit</Button>
+      </Form>
+          
+    </div>
+  );
+};
 export default AddAMilestone;
-
-
-
-
